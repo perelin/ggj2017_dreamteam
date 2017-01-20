6757f11ee0ca3fdcaa0c5726f57caf6af38185c5
Angelo Aracri <angeloaracri@yahoo.de>
2017-01-20 23:11:38 +0100

    Added demo movement on pre-defined path

---
+++ DreamTeam/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public BezierCurve curve;
+
+	// Use this for initialization
+	void Start () {
+		
+		if (curve == null) {
+			Debug.LogError ("Did not specify BezierCurve for Player");
+		}
+	}
+
+
+	private float progress = 0;
+	// Update is called once per frame
+	void Update () {
+		progress = progress <= 1 ? progress + 0.01f : 0;
+
+		this.gameObject.transform.position = curve.GetPointAt (progress);
+
+
+	}
+}
