8ef32383da8f82276e57f3dafe450ebdbb8462e7
Angelo Aracri <angeloaracri@yahoo.de>
2017-01-21 14:28:01 +0100

    Committed.

--- DreamTeam/Assets/LevelBackgroundScript.cs
+++ DreamTeam/Assets/LevelBackgroundScript.cs
@@ -7,7 +7,9 @@ public class LevelBackgroundScript : MonoBehaviour {
 	void Start() {
 		Vector2 scale = this.gameObject.transform.localScale;
 		int width = (int) (scale.x / scale.y);
+
 		this.gameObject.GetComponent<Renderer> ().material.mainTextureScale = new Vector2 (width, 1);
+
 	}
 
 }
