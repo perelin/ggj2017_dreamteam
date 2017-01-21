092f196ac6dff54ddb587d8190a7d0a30ecd0552
bitowl <github@bitowl.de>
2017-01-21 16:42:25 +0100

    Add camera zoom

---
+++ DreamTeam/Assets/Editor/CameraManagerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(CameraManager))]
+public class CameraManagerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        CameraManager myTarget = (CameraManager)target;
+
+        myTarget.Size = EditorGUILayout.FloatField("Zoom", myTarget.Size);
+        if (myTarget.Size < 3)
+        {
+            myTarget.Size = 3;
+        }
+        if (myTarget.Size > 50)
+        {
+            myTarget.Size = 50;
+        }
+    }
+}
--- DreamTeam/Assets/Scripts/Cameras/CameraManager.cs
+++ DreamTeam/Assets/Scripts/Cameras/CameraManager.cs
@@ -11,8 +11,25 @@ public class CameraManager : MonoBehaviour
     public Camera LightsCamera;
     public Camera MainCamera;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private float _size;
+    public float Size
+    {
+        set
+        {
+            LightnessCamera.orthographicSize = value;
+            LightsCamera.orthographicSize = value;
+            MainCamera.orthographicSize = value;
+            _size = value;
+        }
+        get
+        {
+            return _size;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
         // flip the cameras for direct X ...
       /*  if (!(SystemInfo.graphicsDeviceType == GraphicsDeviceType.Direct3D11 ||
             SystemInfo.graphicsDeviceType == GraphicsDeviceType.Direct3D12 ||
@@ -24,12 +41,12 @@ public class CameraManager : MonoBehaviour
             LightsCamera.transform.localPosition = new Vector3(0, 0, 10);
         }
         else
-        {*/
+        {
             LightnessCamera.transform.localRotation = Quaternion.Euler(0, 0, 0);
             LightnessCamera.transform.localPosition = new Vector3(0, 0, -10);
             LightsCamera.transform.localRotation = Quaternion.Euler(0, 0, 0);
             LightsCamera.transform.localPosition = new Vector3(0, 0, -10);
-        //}
+        }*/
     }
 	
 	// Update is called once per frame
