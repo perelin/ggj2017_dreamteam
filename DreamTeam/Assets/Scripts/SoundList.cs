3e3f308741bb0327e19ddd05513181e898d14f3d
Angelo Aracri <angeloaracri@yahoo.de>
2017-01-21 20:03:26 +0100

    Added SoundLists to sort six by genre

---
+++ DreamTeam/Assets/Scripts/SoundList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundList : MonoBehaviour {
+	// sound clip lists
+	public AudioClip[] clips;
+
+	public void Play() {
+		SoundSystem.instance.RandomizeSfx (clips);
+	}
+}
