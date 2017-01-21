5146ab1480e9a7e475d30b908b7e42246c4b05ce
bitowl <github@bitowl.de>
2017-01-21 18:34:25 +0100

    fog reveal by eye + follow line with eye

--- DreamTeam/Assets/Scripts/MouseIlluminator.cs
+++ DreamTeam/Assets/Scripts/MouseIlluminator.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
+using Tobii.EyeTracking;
+
 public class MouseIlluminator : MonoBehaviour
 {
     public Transform Lights;
@@ -10,6 +12,8 @@ public class MouseIlluminator : MonoBehaviour
     private float _count;
     public float SpawnNewLightAfterSeconds = 0.1f;
     public Camera Camera;
+
+    public bool UseEyeTracker = true;
 
 	// Use this for initialization
 	void Start () {
@@ -24,9 +28,28 @@ public class MouseIlluminator : MonoBehaviour
 	    {
 	        _count -= SpawnNewLightAfterSeconds;
 	        var light = Instantiate(LightPoint, Lights);
-            var mousePos = Input.mousePosition;
-	        var objectPos = Camera.ScreenToWorldPoint(mousePos);
-	        light.transform.position = objectPos;
+
+	        Vector3 lookPos = Vector3.zero;
+
+	        if (UseEyeTracker)
+	        {
+                // get eye tracking point
+                GazePoint gazePoint = EyeTracking.GetGazePoint();
+                if (gazePoint.IsValid)
+                {
+                    lookPos = new Vector3(gazePoint.Screen.x, gazePoint.Screen.y);
+                }
+            }
+            else
+	        {
+                // get mouse position as replacement for eye tracking
+	            lookPos = Input.mousePosition;
+	            
+	        }
+
+            var objectPos = Camera.ScreenToWorldPoint(lookPos);
+
+            light.transform.position = objectPos;
 	        light.transform.localPosition -= new Vector3(0, 0, light.transform.localPosition.z);
 
 	    }
