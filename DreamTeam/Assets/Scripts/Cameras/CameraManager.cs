e2655e12cd0b54d9f72075731b6cb3e0faea351a
Angelo Aracri <angeloaracri@yahoo.de>
2017-01-21 22:58:10 +0100

    Added references

--- DreamTeam/Assets/Scripts/Cameras/CameraManager.cs
+++ DreamTeam/Assets/Scripts/Cameras/CameraManager.cs
@@ -32,7 +32,7 @@ public class CameraManager : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        _zoom = MainCamera.orthographicSize;
+        _zoom = 5;
 
         // flip the cameras for direct X ...
         /*  if (!(SystemInfo.graphicsDeviceType == GraphicsDeviceType.Direct3D11 ||
