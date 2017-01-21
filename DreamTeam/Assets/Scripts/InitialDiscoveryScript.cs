d12977c569f987bc913934d103f1e5a9a2d2370d
bitowl <github@bitowl.de>
2017-01-21 23:38:53 +0100

    Add initial discovery

---
+++ DreamTeam/Assets/Scripts/InitialDiscoveryScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialDiscoveryScript : MonoBehaviour
+{
+    public float TimeToAction;
+    public SpriteRenderer SpriteRenderer;
+    public float LerpTime;
+    
+	// Use this for initialization
+	void Start ()
+	{
+        SpriteRenderer.color = new Color(0,0,0,0);
+
+	    var discoveryScript = gameObject.AddComponent<DiscoveryScript>();
+	    discoveryScript.timeToAction = TimeToAction;
+
+	    var discoveryAction = gameObject.AddComponent<InitialDiscoveryAction>();
+	    discoveryAction.LerpTime = LerpTime;
+	    discoveryAction.SpriteRenderer = SpriteRenderer;
+	    discoveryScript.actionToPerform = discoveryAction;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
