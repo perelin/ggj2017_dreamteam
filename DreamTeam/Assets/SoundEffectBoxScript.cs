424051d9551382effc8ca2ab190af82d7cf4aa90
Angelo Aracri <angeloaracri@yahoo.de>
2017-01-21 19:31:17 +0100

    Created Prefab SoundEffectBox, used it in Tower

---
+++ DreamTeam/Assets/SoundEffectBoxScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectBoxScript : MonoBehaviour {
+
+	public AudioClip clip;
+	public float deltaTime;
+
+
+	private float currentDeltaTimeCounter;
+
+	private bool playEffect = false;
+
+	void OnTriggerEnter(Collider other) {
+		playEffect = true;
+	}
+
+	void OnTriggerExit(Collider other) {
+		playEffect = false;
+	}
+
+	void Update() {
+		if (playEffect) {
+			currentDeltaTimeCounter += Time.deltaTime;
+			if (currentDeltaTimeCounter > deltaTime) {
+				currentDeltaTimeCounter = 0;
+				SoundSystem.instance.PlaySingle (clip);
+			}
+		}
+	}
+}
