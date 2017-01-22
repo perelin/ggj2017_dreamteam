211f9c27fe8aaf0b56601aa9eddf2edad283990f
Angelo Aracri <angeloaracri@yahoo.de>
2017-01-22 02:44:16 +0100

    Added spinning fish

---
+++ DreamTeam/Assets/Scripts/FishTurnScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishTurnScript : MonoBehaviour {
+	
+	public Vector3 middle;
+	public float radius;
+
+	public float progressSpeed;
+
+	private float currentProgress;
+	
+	// Update is called once per frame
+	void Update () {
+
+		currentProgress += progressSpeed * Time.deltaTime;
+
+		float inner = currentProgress * 2 * Mathf.PI;
+
+		this.gameObject.transform.localPosition = middle + new Vector3 (-radius * Mathf.Sin(inner), radius * Mathf.Cos(inner), 0);
+		this.gameObject.transform.localEulerAngles = new Vector3 (0, 0, currentProgress*360+180);
+
+	}
+}
