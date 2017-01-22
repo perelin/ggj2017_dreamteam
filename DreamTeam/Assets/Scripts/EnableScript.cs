2403f0e397949da2d249a81094c48a1c4fdea6e2
bitowl <github@bitowl.de>
2017-01-22 15:00:53 +0100

    repair torch scene

---
+++ DreamTeam/Assets/Scripts/EnableScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableScript : MonoBehaviour
+{
+
+    public GameObject[] GameObjects;
+    public Behaviour[] Behaviours;
+
+    void PerformAction()
+    {
+        foreach (var go in GameObjects)
+        {
+            go.SetActive((true));
+        }
+
+        foreach (var bhv in Behaviours)
+        {
+            bhv.enabled = true;
+        }
+    }
+}
