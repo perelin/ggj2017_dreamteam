a1be911c7f5964c9e10ea676b502a8a18a8a7820
Angelo Aracri <angeloaracri@yahoo.de>
2017-01-22 07:51:17 +0100

    Added spiral asset to game

---
+++ DreamTeam/Assets/Scripts/SpinningScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningScript : MonoBehaviour {
+
+	public float Speed;
+
+
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.eulerAngles = new Vector3 (0, 0, this.gameObject.transform.eulerAngles.z - Speed * Time.deltaTime);
+	}
+}
